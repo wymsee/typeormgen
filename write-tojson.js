2197be3005d82df79d248a486b9b3c394f01a1cb
Nate Almeida <nate.almeida@gmail.com>
2018-03-12 13:23:22 -0400

    Added optional toJSON method generation

---
+++ write-tojson.js
@@ -0,0 +1,22 @@
+const { forbiddenExportCols } = require('./constants');
+
+module.exports = function(info, tab) {
+	let content = `${tab}toJSON() {
+${tab.repeat(2)}return {
+`;
+	
+	Object.keys(info).forEach(col => {
+		if (forbiddenExportCols.indexOf(col) == -1) {
+		content += `${tab.repeat(3)}${col}: this.${col},
+`;
+		}
+	});
+	content = content.slice(0, -2);
+	content += '\n';
+	content += `${tab.repeat(2)}};
+${tab}}
+
+`;
+	console.log(content);
+	return content;
+};
